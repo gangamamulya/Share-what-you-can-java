ccc7b3e7629286f40e81e482f2c1cd7214c86ab6
amulyaAmmu <40600002+amulyaAmmu@users.noreply.github.com>
2022-02-04 17:27:36 -0800

    UI screens 1) Select user type - donor or reciever 2) Donor input screen

---
+++ app/src/main/java/com/example/sharewhatyoucanproject/DonorActivity.java
@@ -0,0 +1,15 @@
+package com.example.sharewhatyoucanproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DonorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donor);
+    }
+}
